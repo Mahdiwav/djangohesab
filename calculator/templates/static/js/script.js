c39e204304aa48601180d5ecc7c3df86409105a3
Mahdiwav <mahdifaghani82@gmail.com>
2024-05-10 12:11:47 +0330

    Optimize codes

---
+++ calculator/templates/static/js/script.js
@@ -0,0 +1,33 @@
+var historyContainer = document.getElementById('calc-history-container');
+var historyBox = document.getElementById('calc-history');
+function updateScroll() {
+  if (historyBox.scrollHeight > historyContainer.clientHeight) {
+    historyContainer.classList.add('scrollable');
+  } else {
+    historyContainer.classList.remove('scrollable');
+  }
+}
+
+window.addEventListener('load', updateScroll);
+
+window.addEventListener('resize', updateScroll);
+function s(v) { document.getElementById('res').value = v }
+function a(v) {
+document.getElementById('res').value += v;
+}
+function e() {
+try {
+    var result = eval(document.getElementById('res').value);
+    document.getElementById('calc-history').innerHTML += "<div>" + document.getElementById('res').value + " = " + result + "</div>";
+    s(result);
+} catch(e) {
+    s('Error');
+}
+}
+function x() {
+    if(document.getElementById('res').value == 1) {
+    document.getElementById('res').value = 0;
+} else {
+    document.getElementById('res').value = document.getElementById('res').value.substring(0, document.getElementById('res').value.length - 1);
+}
+}
--- djangohesab/settings.py
+++ djangohesab/settings.py
@@ -119,7 +119,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "calculator/templates/static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
